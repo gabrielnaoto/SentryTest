f795717741bbbce1d495d51b6fb6cc08364bc4bd
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2021-03-04 23:24:33 -0300

    Install Sentry

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://4484514715374625a0b57a576e6aefa5@o272326.ingest.sentry.io/5662509',
+  enableNative: false,
+});
 
 export default function App() {
+  throw new Error("My first Sentry error!");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
